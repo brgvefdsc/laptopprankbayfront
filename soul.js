f45bc6323679ae1df3432cdc247de6c60837c845
brgvefdsc <91153971+brgvefdsc@users.noreply.github.com>
2022-01-19 20:22:11 -0800

    Update soul.js

--- soul.js
+++ soul.js
@@ -1,4 +1,4 @@
-setInterval(function () {window.open("file:///Users/bayfrontstudent//NeverG.mp4", "_blank")}, 1);
-setInterval(function () {window.open("file:///Users/bayfrontstudent//NeverG.mp4", "_blank")}, 1);
-setInterval(function () {window.open("file:///Users/bayfrontstudent//NeverG.mp4", "_blank")}, 1);
-setInterval(function () {window.open("file:///Users/bayfrontstudent//NeverG.mp4", "_blank")}, 1);
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Movies/NeverG.mp4", "_blank")}, 1);
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Movies/NeverG.mp4", "_blank")}, 1);
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Movies/NeverG.mp4", "_blank")}, 1);
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Moives/NeverG.mp4", "_blank")}, 1);
